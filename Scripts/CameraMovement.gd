40ad16033939f7d7f22d27c94dcd1f87db412e6c
Alex Jobe <ajobe15@gmail.com>
2018-11-30 14:36:38 -0600

    Add camera zoom

--- Scripts/CameraMovement.gd
+++ Scripts/CameraMovement.gd
@@ -1,6 +1,14 @@
 extends Camera2D
 
-export var speed = 10.0
+export var speed = 30.0
+export var zoomspeed = 20.0
+
+export var zoomMin = 0.5
+export var zoomMax = 2
+
+var zoompos = Vector2()
+var zoomfactor = 1.0
+var zooming = false
 
 func _ready():
 	pass
@@ -14,7 +22,37 @@ func _process(delta):
 	var input_y = (int(Input.is_action_pressed("ui_down")) 
 		- int(Input.is_action_pressed("ui_up")))
 	
-	position.x += input_x * speed
-	position.y += input_y * speed
+	position.x = lerp(position.x, position.x + input_x * speed * zoom.x, speed * delta)
+	position.y = lerp(position.y, position.y + input_y * speed * zoom.y, speed * delta)
 	
-	pass
+	# Camera zoom
+	zoom.x = lerp(zoom.x, zoom.x * zoomfactor, zoomspeed * delta)
+	zoom.y = lerp(zoom.y, zoom.y * zoomfactor, zoomspeed * delta)
+	
+	zoom.x = clamp(zoom.x, zoomMin, zoomMax)
+	zoom.y = clamp(zoom.y, zoomMin, zoomMax)
+	
+	if not zooming:
+		zoomfactor = 1.0
+	
+
+func _input(event):
+		
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			zooming = true
+			if event.button_index == BUTTON_WHEEL_UP:
+				zoomfactor -= 0.01 * zoomspeed
+				zoompos = get_global_mouse_position()
+			if event.button_index == BUTTON_WHEEL_DOWN:
+				zoomfactor += 0.01 * zoomspeed
+				zoompos = get_global_mouse_position()
+		else:
+			zooming = false
+			
+			
+			
+			
+			
+			
+			 
