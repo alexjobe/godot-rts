8b1690b4b65bf8a552b2a2d64400e97a58382d18
Alex Jobe <ajobe15@gmail.com>
2018-12-01 13:29:59 -0600

    Add selectable units

---
+++ Scripts/Unit.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+
+export var selected = false setget set_selected
+onready var box = $Box
+
+signal was_selected
+signal was_deselected
+
+func set_selected(value):
+	if selected != value:
+		selected = value
+		box.visible = value
+	if selected:
+		emit_signal("was_selected", self)
+	else:
+		emit_signal("was_deselected", self)
+
+func _ready():
+	connect("was_selected", get_parent(), "select_unit")
+	connect("was_deselected", get_parent(), "deselect_unit")
+
+func _process(delta):
+	pass
+
+func _on_Unit_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			set_selected(true)
+		if event.button_index == BUTTON_RIGHT:
+			set_selected(false)
+		
+		
